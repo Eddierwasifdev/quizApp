500ac895319d3df7f59812f8df8a4a2454ab6399
Eddierwasifdev <phabibon09@gmail.com>
2023-08-10 17:28:44 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Quizmify",
+  title: "Quiz Quick",
   description: "Quiz yourself on anything!",
 };
 
